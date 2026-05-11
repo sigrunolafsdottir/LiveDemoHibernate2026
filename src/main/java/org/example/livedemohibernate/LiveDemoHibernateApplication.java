c23eb38ba53c48e1c2d5d4e539a649d48d01b8cb
sigrunolafsdottir <solafsdottir@gmail.com>
2026-05-11 12:47:45 +0200

    Initial commit

---
+++ src/main/java/org/example/livedemohibernate/LiveDemoHibernateApplication.java
@@ -0,0 +1,13 @@
+package org.example.livedemohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDemoHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveDemoHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/livedemohibernate/LiveDemoHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.livedemohibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDemoHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
